6b35c52fd222302cc6687a68b7f041bd4c5b71e3
Jenifer Fabian Dubon <jenifer_dubon@wc118-dhcp-208.brynmawr.edu>
2022-09-06 16:13:26 -0400

    first assignment

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * Hello.c
+ * Author: Aline Normoyle
+ * Date: 1/11/2022
+ * Print Hello World
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("A journey of a thousand miles begins with a single step.\n");
+  return 0;
+}
