3e0fef8b1051b57b452d12977805efa76f4eb999
Jennifer Fabian Dubon <jfabiandub@goldengate.cs.brynmawr.edu>
2022-10-21 18:21:20 -0400

    Assignment 5 updated

--- A05/glitch.c
+++ A05/glitch.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include <time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
-//v//oid clean(struct ppm_pixel* image, int w, int h){
-  //for(int i =0; i<h; i++){
-    //free(image);
-  //}
-  //free(image);
-//}
 
 int main(int argc, char** argv) {
   if (argc != 2) {
@@ -17,13 +10,13 @@ int main(int argc, char** argv) {
     return 0;
   }
   
-  //srand(time(0));
+  /* holds the width and height */
   int w;
   int h;
 
   char* filename = argv[1];
   struct ppm_pixel* pixels;
-  pixels = read_ppm(filename, &w, &h);
+  pixels = read_ppm(filename, &w, &h);  //reads the file, width and height
   printf("%d\n", h);
   if(pixels == NULL){
     printf("Error, not a valid file\n");
@@ -31,6 +24,7 @@ int main(int argc, char** argv) {
   }
   printf("Reading %s with width %d and heigth %d\n", filename, w, h);
 
+  /* goes through the image and shifts each color value by either 1 or 2 bits at random   */
   for(int i = 0; i< h; i++){
     for (int j= 0; j<w; j++){
       struct ppm_pixel p = pixels[i*w +j];
@@ -42,22 +36,17 @@ int main(int argc, char** argv) {
   }
 
 char* new_fp = malloc(strlen(filename)+8); //this should save the new file with the new name
+ 
+ /* saves the new file with a new name */
  strcpy(new_fp, filename);
  new_fp[strlen(new_fp)-4] = '\0';
  strcat(new_fp, "-glitch.ppm");
  printf("Writing file %s\n", new_fp);
  
-
-  //printf("Reading %s with width %d and heigth %d\n", argv[1], w, h);
+ //modifies the file 
   write_ppm(new_fp, pixels,w, h);
   
   free(pixels);
-  //pixels = read_ppm(new_fp, &w, &h);
- // printf("Writing file %s", new_fp);
-
-
- //fclose(filename);
- //clean(pixels, w, h);
 
   free(new_fp);
 
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -5,16 +5,19 @@
 // implement one
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+    /*  reads the file  */
 FILE *fp;
-    fp = fopen(filename, "w");
+    fp = fopen(filename, "w"); //opens the fille
     if(fp ==NULL){
         printf("Error opening file. %s\n", filename);
         return;
     }
+    /*  writes each of the arguments */
+    fprintf(fp, "P6 \n");   //type
+    fprintf( fp," %d %d\n", w, h);  //width and height
+    fprintf(fp, "2555\n");  
 
-    fprintf(fp, "P6 \n");
-    fprintf( fp," %d %d\n", w, h);
-    fprintf(fp, "2555\n");
+    /*  goes through the file and writes the pixels for red, blue and green */
     for(int i = 0; i < h; i++){
         for(int j = 0; j < w; j++){
             fwrite(&pixels[i*w + j].red, 1, 1, fp);
